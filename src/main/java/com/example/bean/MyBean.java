4a1d35d5cf7c5c61ca1c461dfb7a983136db3524
@lokeshgithu <lokeshgithub786@gmail.com>
2025-04-26 12:09:16 +0530

    (id-003) Creating first bean of the application.

---
+++ src/main/java/com/example/bean/MyBean.java
@@ -0,0 +1,21 @@
+package com.example.bean;
+
+public class MyBean {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void showMessage(){
+        System.out.println("message: "+message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
