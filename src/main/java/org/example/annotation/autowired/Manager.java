35eb6cf20b68e68b05be3a520ce1e5db1e539c7d
@lokeshgithu <lokeshgithub786@gmail.com>
2025-04-28 09:17:30 +0530

    (id-014) making use of @Qualifier to inject dependency to config in spring.

--- src/main/java/org/example/annotation/autowired/Manager.java
+++ src/main/java/org/example/annotation/autowired/Manager.java
@@ -1,17 +1,20 @@
 package org.example.annotation.autowired;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Manager {
+    @Autowired
+    @Qualifier("employee")
     private  Employee employee;
-
+/*
     @Autowired
     public Manager(Employee employee) {
         this.employee = employee;
     }
-
+*/
     @Override
     public String toString() {
         return "Manager{" +
