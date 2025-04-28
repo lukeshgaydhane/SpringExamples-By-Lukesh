96ce923e6564751dc5fe4e085b7f7e59ab326cb6
@lokeshgithu <lokeshgithub786@gmail.com>
2025-04-28 08:57:24 +0530

    Revert "(id-012) making use of @Autowired annotation to automatically inject dependency to config in spring."
    This reverts commit 63e4af89bf864e89b7939773aa7dcfc318f8b696.

--- src/main/java/org/example/annotation/autowired/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package org.example.annotation.autowired;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "org.example.annotation.autowired")
-public class AppConfig {
-}
